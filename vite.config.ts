41caf60606dc0485a5f92fb5bca962d749237f5a
Mateoac1998 <161606098+Mateoac1998@users.noreply.github.com>
2026-09-05 12:23:34 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/wandra.com.co/',
   plugins: [react()],
   resolve: {
     alias: {
