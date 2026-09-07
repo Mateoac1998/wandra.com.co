f34c302027e67b0ca04eafe6339550d06951bb41
Mateoac1998 <161606098+Mateoac1998@users.noreply.github.com>
2026-09-07 10:32:50 -0500

    Update vite.config.ts
    ajuste de jerarquias

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/wandra.com.co/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
